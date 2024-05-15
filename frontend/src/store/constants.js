5e48cb485794f1a9ebcf4b67b14df542c87f9917
solomon <slegesse2444@gmail.com>
2024-05-15 16:31:40 +0300

    complete profile pending and report

--- frontend/src/store/constants.js
+++ frontend/src/store/constants.js
@@ -2,3 +2,4 @@ export const BASE_URL = ""
 export const USER_URL = `/api/users`
 export const HOUSE_URL = `/api/house`
 export const PENDING_URL = `/api/pendingOrder`
+export const Report_URL = `/api/report`

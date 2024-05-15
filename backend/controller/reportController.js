d9d5ab87e33d6e4a9f9b1ec81de3abdf027de741
solomon <slegesse2444@gmail.com>
2024-05-15 15:50:32 +0300

    update reportController

--- backend/controller/reportController.js
+++ backend/controller/reportController.js
@@ -1,4 +1,5 @@
 import Report from "../model/ReportModel.js"
+import House from "../model/houseModel.js"
 import asyncHandler from "express-async-handler"
 
 const createReport = asyncHandler(async (req, res) => {
@@ -14,13 +15,62 @@ const createReport = asyncHandler(async (req, res) => {
   res.status(201).json(createdReport)
 })
 
-const getReportsForhouse = asyncHandler(async (req, res) => {
-  const reports = await Report.find({ house: req.params.id })
+const getallReports = asyncHandler(async (req, res) => {
+  const pageSize = 8
+  const page = Number(req.query.pageNumber) || 1
+  const count = await Report.countDocuments({})
+  const reports = await Report.find({})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
   if (!reports) {
     res.status(404)
     throw new Error("Reports not found")
   }
-  res.status(200).json(reports)
+  res.status(200).json({
+    reports,
+    page,
+    pages: Math.ceil(count / pageSize),
+  })
+})
+
+const getReportsForhouse = asyncHandler(async (req, res) => {
+  const pageSize = 8
+  const page = Number(req.query.pageNumber) || 1
+  const queryObj =
+    req.user.role === "landlord"
+      ? {
+          hasBroker: false,
+          user: req.user._id,
+          active: true,
+        }
+      : {
+          active: true,
+          $or: [
+            { user: req.user._id },
+            {
+              brokers: {
+                $elemMatch: {
+                  broker: req.user._id,
+                  status: "accepted",
+                },
+              },
+            },
+          ],
+        }
+  const houses = await House.find(queryObj)
+  const count = await Report.countDocuments({ house: { $in: houses } })
+  const reports = await Report.find({ house: { $in: houses } })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1))
+  if (!reports) {
+    res.status(404)
+    throw new Error("Reports not found")
+  }
+  res.status(200).json({
+    reports,
+    page,
+    pages: Math.ceil(count / pageSize),
+  })
 })
 
 const getMyReports = asyncHandler(async (req, res) => {
@@ -39,9 +89,20 @@ const resolveReport = asyncHandler(async (req, res) => {
     throw new Error("Report not found")
   }
 
+  if (report.status === "resolved") {
+    res.status(400)
+    throw new Error("Report already resolved")
+  }
+
   report.status = "resolved"
   const updatedReport = await report.save()
   res.status(200).json(updatedReport)
 })
 
-export { createReport, getReportsForhouse, getMyReports, resolveReport }
+export {
+  createReport,
+  getReportsForhouse,
+  getMyReports,
+  getallReports,
+  resolveReport,
+}
